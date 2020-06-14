1ce67c06a08ab268ceda6f97764b9fe3e0e2aabc
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-06-14 19:18:56 +0530

    minor restructuring to fit the api needs for cache

--- inc/cache.h
+++ inc/cache.h
@@ -11,17 +11,27 @@ typedef struct cache cache;
 struct cache
 {
 	pthread_mutex_t	free_list_lock;		// allows mutually exclusive access to the free slabs list
-	pthread_mutex_t	partial_list_lock;	// allows mutually exclusive access to the partial slabs list
-	pthread_mutex_t full_list_lock; 	// allows mutually exclusive access to the full slabs list
-
 	slab_desc* free;			// list of free slabs ready to reap, no objects are currently in user space from here
+	uint32_t free_slabs;		// number of slabs in 
+
+	pthread_mutex_t	partial_list_lock;	// allows mutually exclusive access to the partial slabs list
 	slab_desc* partial;			// list of partial slabs, for further allocation
+	uint32_t partial_slabs;		// number of partial slabs
+
+	pthread_mutex_t full_list_lock; 	// allows mutually exclusive access to the full slabs list
 	slab_desc* full;			// list of full slabs, a slab is moved to-fro free and full slabs as required
+	uint32_t full_slabs;		// number of full slabs
+
+	uint32_t min_in_use_slabs;	// if the number of partial + full slabs geos below this number 
+								// a new slab is moved from free to partial slabs and the new object is allocated from there
+	uint32_t max_free_slabs;	// free slabs are automatically returned if they exceeed max_free_slabs
 
 	size_t slab_size;			// size of each slab in bytes, must always be multiple of 4096 and 
 								// greater than atleast 32 times the size of the object
 
 	size_t object_size;			// size of each object in bytes, this must always be a multiple of 64
+
+	volatile int growing;		// set when a new slab is being created and will be added to free list soon
 
 	// number_of_objects_per_slab = ( (8*(slab_size-sizeof(slab_desc))) / ((8*object_size)+1))
 	// 1 bit for allocation mapping, to check if a object is allocated or not
@@ -38,7 +48,7 @@ int cache_create(cache* cachep, size_t slab_size, size_t object_size, void (*ini
 void* cache_alloc(cache* cachep);
 void cache_free(cache* cachep, void* obj);
 
-int cache_grow(cache* cachep);
+int cache_grow(cache* cachep, uint32_t slabs_to add);
 int cache_reap(cache* cachep);
 
 int cache_destroy(cache* cachep);
