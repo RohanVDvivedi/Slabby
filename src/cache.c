ce3deff332461848d29a6c0ccac4c36ebe168e49
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-08-10 12:29:29 +0530

    removed an access to out of bounds slab

--- src/cache.c
+++ src/cache.c
@@ -94,7 +94,7 @@ void* cache_alloc(cache* cachep)
 
 	// get any one slab from the first one third of the slab list, this lessens the lock contention over the same slab by different threads
 	// at the same time we aim to not finish up all the slabs in the partial list at the same time
-	slab_desc* slab_desc_p = (slab_desc*) get_nth_from_head(&(cachep->partial_slab_descs), ((unsigned int)pthread_self()) % ((cachep->partial_slabs/3) + 1));
+	slab_desc* slab_desc_p = (slab_desc*) get_nth_from_head(&(cachep->partial_slab_descs), ((unsigned int)pthread_self()) % (cachep->partial_slabs/3));
 
 	// lock the slab asap after you get the pointer to it
 	lock_slab(slab_desc_p);
