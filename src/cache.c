be5f484c2295de8e26c1b03ea67c827281c885e5
rohan <rohandvivedi@gmail.com>
2022-01-26 13:22:12 -0700

    minor bug fix and removed transfer from one linkedlist to another functions, they ae now not needed

--- src/cache.c
+++ src/cache.c
@@ -16,18 +16,6 @@ unsigned int number_of_objects_per_slab(cache* cachep)
 	return ( 8 * (cachep->slab_size - sizeof(slab_desc)) ) / (8 * cachep->object_size + 1);
 }
 
-static void transfer_a_to_b_head(const void* slab_desc_p, linkedlist* a, linkedlist* b)
-{
-	remove_from_linkedlist(a, slab_desc_p);
-	insert_head_in_linkedlist(b, slab_desc_p);
-}
-
-static void transfer_a_to_b_tail(const void* slab_desc_p, linkedlist* a, linkedlist* b)
-{
-	remove_from_linkedlist(a, slab_desc_p);
-	insert_tail_in_linkedlist(b, slab_desc_p);
-}
-
 static void cache_grow_unsafe(cache* cachep)
 {
 	slab_desc* slab_desc_p = slab_create(cachep);
@@ -93,8 +81,10 @@ void* cache_alloc(cache* cachep)
 		}
 
 		// transfer only one slab from free to partial
-		transfer_a_to_b_head(get_head_of_linkedlist(&(cachep->free_slab_descs)), &(cachep->free_slab_descs), &(cachep->partial_slab_descs));
+		const slab_desc* slab_desc_to_move = get_head_of_linkedlist(&(cachep->free_slab_descs));
+		remove_from_linkedlist(&(cachep->free_slab_descs), slab_desc_to_move);
 		cachep->free_slabs--;
+		insert_head_in_linkedlist(&(cachep->partial_slab_descs), slab_desc_to_move);
 		cachep->partial_slabs++;
 	}
 
@@ -109,8 +99,9 @@ void* cache_alloc(cache* cachep)
 	// if there is only one object, on the slab allocate it and mave the slab to full list
 	if(slab_desc_p->free_objects == 1)
 	{
-		transfer_a_to_b_tail(slab_desc_p, &(cachep->partial_slab_descs), &(cachep->full_slab_descs));
+		remove_from_linkedlist(&(cachep->partial_slab_descs), slab_desc_p);
 		cachep->partial_slabs--;
+		insert_tail_in_linkedlist(&(cachep->full_slab_descs), slab_desc_p);
 		cachep->full_slabs++;
 	}
 
@@ -146,16 +137,18 @@ int cache_free(cache* cachep, void* obj)
 	// if it is in full slabs description, move it to the end of the partial list
 	if(exists_in_full_slabs)
 	{
-		transfer_a_to_b_tail(slab_desc_p, &(cachep->full_slab_descs), &(cachep->partial_slab_descs));
+		remove_from_linkedlist(&(cachep->full_slab_descs), slab_desc_p);
 		cachep->full_slabs--;
+		insert_tail_in_linkedlist(&(cachep->partial_slab_descs), slab_desc_p);
 		cachep->partial_slabs++;
 	}
 	else if(exists_in_partial_slabs)
 	{
 		if(slab_desc_p->free_objects == number_of_objects_per_slab(cachep) - 1)
 		{
-			transfer_a_to_b_tail(slab_desc_p, &(cachep->partial_slab_descs), &(cachep->free_slab_descs));
+			remove_from_linkedlist(&(cachep->partial_slab_descs), slab_desc_p);
 			cachep->partial_slabs--;
+			insert_tail_in_linkedlist(&(cachep->free_slab_descs), slab_desc_p);
 			cachep->free_slabs++;
 		}
 	}
